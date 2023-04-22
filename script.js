8223f44b46a9ff52ab46e60cfbe09a29d1ed2bf9
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-22 18:18:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let x = document.getElementById('x');
+
+let y = document.getElementById('y');
+function reportWindowSize() {
+  x.textContent = window.innerWidth;
+  y.textContent = window.innerHeight;
+}
+window.addEventListener('resize',reportWindowSize)
+
+window.onresize = reportWindowSize;
